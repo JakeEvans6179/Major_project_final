5578184d319636ec06944b88a120a9fffbc0b8bb
JakeEvans6179 <jakeyboyhk@icloud.com>
2026-04-21 02:47:02 +0100

    commit

--- 05_federated/LSTM64x32/lstm64x32_local_fine_tuning.py
+++ 05_federated/LSTM64x32/lstm64x32_local_fine_tuning.py
@@ -29,7 +29,7 @@ data_path = Path("../data_files/final_locked_100_normalised.parquet")
 max_min_path = Path("../data_files/global_weather_scaler.csv")
 local_kwh_scaling = Path("../data_files/local_kwh_scaler.csv")
 
-global_model_path = Path("chunk_checkpoints/global_chunk_034_LSTM64x32.keras")  #find the best model from validation screening and use for fine tuning
+global_model_path = Path("chunk_checkpoints/global_chunk_094_LSTM64x32.keras")  #find the best model from validation screening and use for fine tuning
 
 
 HORIZON = 6
